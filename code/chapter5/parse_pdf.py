dbc02140c5003b29a8681eae79ce924412497417
Aman-Garg <amangarg210@gmail.com>
2019-03-07 02:30:34 +0530

    checking rsa key

--- code/chapter5/parse_pdf.py
+++ code/chapter5/parse_pdf.py
@@ -3,6 +3,6 @@ import slate
 with open('../../data/chapter5/EN-FINAL Table 9.pdf','rb') as f:
     doc = slate.PDF(f)
 
-#this is not yet complete trying to find some other module
+#this is not yet complete trying to find some other module asad
 for page in doc[:2]:
     print(page)
