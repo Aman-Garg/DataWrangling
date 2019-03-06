6c33a8a765a8316e4ec0c0546dbbe1f03f514756
Aman-Garg <amangarg210@gmail.com>
2019-03-07 02:38:07 +0530

    checking ssh key

--- code/chapter5/parse_pdf.py
+++ code/chapter5/parse_pdf.py
@@ -3,6 +3,6 @@ import slate
 with open('../../data/chapter5/EN-FINAL Table 9.pdf','rb') as f:
     doc = slate.PDF(f)
 
-#this is not yet complete trying to find some other module asad
+#this is not yet complete trying to find some other module 
 for page in doc[:2]:
     print(page)
